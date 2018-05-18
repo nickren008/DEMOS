46c21ed36301d2af391efced4bba03b905c90e27
github's Namefpgdev <Nick@NickdeMacBook-Pro-2.local>
2018-05-18 11:46:02 +0800

    New

---
+++ 读取jason数据/data.js
@@ -0,0 +1,44 @@
+var data  = [
+    {
+    'name' : 'frontend',
+    'list' : [
+        {
+            'title' : 'developer',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        },
+        {
+            'title' : 'PM',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        },
+        {
+            'title' : 'QA',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        }
+    ]
+},
+{
+    'name' : 'backend',
+    'list' : [
+        {
+            'title' : 'developer',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        },
+        {
+            'title' : 'PM',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        },
+        {
+            'title' : 'QA',
+            'company' : 'Time Inc',
+            'data' : '2017-5-14'
+        }
+    ]
+}
+
+]
+
